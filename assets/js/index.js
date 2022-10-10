944630defae2f77ba771e634194eb28582d868c5
Dandy_loco <113280098+dandyloco@users.noreply.github.com>
2022-10-10 19:50:43 +0200

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $('#buscador').keyup(function(){
+     var nombres = $('.nombres');
+     var buscando = $(this).val();
+     var item='';
+     for( var i = 0; i < nombres.length; i++ ){
+         item = $(nombres[i]).html().toLowerCase();
+          for(var x = 0; x < item.length; x++ ){
+              if( buscando.length == 0 || item.indexOf( buscando ) > -1 ){
+                  $(nombres[i]).parents('.machine').show(); 
+              }else{
+                   $(nombres[i]).parents('.machine').hide();
+              }
+          }
+     }
+  });
+});
